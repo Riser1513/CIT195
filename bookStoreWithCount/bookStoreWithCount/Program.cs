36dc7f5b72ec50974997b8396009b32bbd773ba4
Riser1513 <37090416+Riser1513@users.noreply.github.com>
2025-03-03 15:15:12 -0500

    Lesson 2

---
+++ bookStoreWithCount/bookStoreWithCount/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace bookStore
+{
+    class myStore
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+            book1.SetTransactions();
+
+            book1.SetId(5690);
+            book1.SetAuthor("Mango Juice");
+            book1.SetTitle("Squirtle is Himself");
+
+            Book book2 = new Book();
+
+            book2.SetTransactions();
+
+            Console.WriteLine("Please enter a Book ID number: ");
+            book2.SetId(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Please enter a Book Name: ");
+            book2.SetTitle(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Book Author: ");
+            book2.SetAuthor(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Book book3 = new Book(9840, "Chaquerie", "A Random Gal");
+
+            book3.SetTransactions();
+
+            Console.WriteLine("Please enter a Book ID number: ");
+            int tempId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Book Name: ");
+            string tempTitle = Console.ReadLine();
+
+            Console.WriteLine("Please enter a Book Author: ");
+            string tempAuthor = Console.ReadLine();
+
+            Book book4 = new Book(tempId, tempTitle, tempAuthor);
+
+            book4.SetTransactions();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"The bookstore has {book1.GetTransactions()} books.");
+
+            displayBooks(book1);
+
+            displayBooks(book2);
+
+            displayBooks(book3);
+
+            displayBooks(book4);
+        }
+
+        public static void displayBooks(Book book)
+        {
+            Console.WriteLine("Here's a book's information: ");
+            Console.WriteLine($"ID Number: {book.GetId()}");
+            Console.WriteLine($"Title: {book.GetTitle()}");
+            Console.WriteLine($"Author: {book.GetAuthor()}");
+            Console.WriteLine();
+        }
+    }
+
+    class Book
+    {
+        private int _Id;
+        private string _Title;
+        private string _Author;
+        private static int _Transactions = 0;
+        public Book()
+        {
+            _Id = 79;
+            _Title = "McDonald's Training Manual";
+            _Author = "Some Guy";
+        }
+        public Book(int id, string title, string author)
+        {
+            _Id = id;
+            _Title = title;
+            _Author = author;
+        }
+
+        public int GetId()
+        {
+            return _Id;
+        }
+
+        public void SetId(int id)
+        {
+            _Id = id;
+        }
+
+        public string GetTitle()
+        {
+            return _Title;
+        }
+
+        public void SetTitle(string title)
+        {
+            _Title = title;
+        }
+
+        public string GetAuthor()
+        {
+            return _Author;
+        }
+
+        public void SetAuthor(string author)
+        {
+            _Author = author;
+        }
+
+        public int GetTransactions()
+        {
+            return _Transactions;
+        }
+
+        public void SetTransactions()
+        {
+            _Transactions += 1;
+        }
+    }
+}
---
+++ myProject/myProject/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace myProject
+{
+    class Game
+    {
+        private string _Title;
+        private string _Developer;
+        private static int _Copies = 0;
+
+        public Game(string title, string developer)
+        {
+            _Title = title;
+            _Developer = developer;
+        }
+
+        public Game()
+        {
+
+        }
+
+        public string GetTitle()
+        {
+            return _Title;
+        }
+
+        public void SetTitle(string title)
+        {
+            _Title = title;
+        }
+
+        public string GetDev()
+        {
+            return _Developer;
+        }
+
+        public void SetDev(string dev)
+        {
+            _Developer = dev;
+        }
+
+        public int GetCopies()
+        {
+            return _Copies;
+        }
+
+        public void SetCopies()
+        {
+            _Copies += 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1 = new Game();
+
+            game1.SetCopies();
+            game1.SetTitle("Terraria");
+            game1.SetDev("Re-Logic");
+
+            Game game2 = new Game("Magic","Framility");
+
+            game2.SetCopies();
+
+            Game game3 = new Game();
+
+            game3.SetCopies();
+
+            Console.WriteLine("Type a Title of a game: ");
+            game3.SetTitle(Console.ReadLine());
+            Console.WriteLine("Type a Developer of the game: ");
+            game3.SetDev(Console.ReadLine());
+
+            Game game4 = new Game("Klimitragha","IDK, Man");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"There are {game1.GetCopies()} copies.");
+            printGame(game1);
+            printGame(game2);
+            printGame(game4);
+            printGame(game3);
+        }
+
+        public static void printGame(Game game)
+        {
+            Console.WriteLine("The Game is as follows: ");
+            Console.WriteLine($"The Title is {game.GetTitle()}");
+            Console.WriteLine($"The Developer is {game.GetDev()}");
+            Console.WriteLine();
+        }
+    }
+}
