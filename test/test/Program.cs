a06b08db6032a517eed5a2f4bec99c85ff22ea76
Riser1513 <37090416+Riser1513@users.noreply.github.com>
2025-03-03 22:59:10 -0500

    test

---
+++ test/test/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        enum Days:short { Sunday, Monday = 11, Tuesday = 10, Wednesday, Thursday, Friday, Saturday }
+        static void Main(string[] args)
+        {
+                        Days today = 0;
+                        Console.WriteLine(today);
+                        today = (Days)12;
+                        Console.WriteLine(today);
+        }
+    }
+}
