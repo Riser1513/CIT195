f8a4966fd062be16b4fbe67f604afcb43d4fa6d0
Riser1513 <37090416+Riser1513@users.noreply.github.com>
2025-03-30 10:40:18 -0400

    Lesson 8 Prt 2

---
+++ famousPeople/famousPeople/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace famousPeople
+{
+    internal class Program
+    {
+        class famousPeople
+        {
+            public string Name { get; set; }
+            public int? BirthYear { get; set; } = null;
+            public int? DeathYear { get; set; } = null;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            IList<famousPeople> stemPeople = new List<famousPeople>() {
+                new famousPeople() { Name= "Michael Faraday", BirthYear=1791,DeathYear=1867 },
+                new famousPeople() { Name= "James Clerk Maxwell", BirthYear=1831,DeathYear=1879 },
+                new famousPeople() { Name= "Marie Skłodowska Curie", BirthYear=1867,DeathYear=1934 },
+                new famousPeople() { Name= "Katherine Johnson", BirthYear=1918,DeathYear=2020 },
+                new famousPeople() { Name= "Jane C. Wright", BirthYear=1919,DeathYear=2013 },
+                new famousPeople() { Name = "Tu YouYou", BirthYear= 1930 },
+                new famousPeople() { Name = "Françoise Barré-Sinoussi", BirthYear=1947 },
+                new famousPeople() {Name = "Lydia Villa-Komaroff", BirthYear=1947},
+                new famousPeople() {Name = "Mae C. Jemison", BirthYear=1956},
+                new famousPeople() {Name = "Stephen Hawking", BirthYear=1942,DeathYear=2018 },
+                new famousPeople() {Name = "Tim Berners-Lee", BirthYear=1955 },
+                new famousPeople() {Name = "Terence Tao", BirthYear=1975 },
+                new famousPeople() {Name = "Florence Nightingale", BirthYear=1820,DeathYear=1910 },
+                new famousPeople() {Name = "George Washington Carver", DeathYear=1943 },
+                new famousPeople() {Name = "Frances Allen", BirthYear=1932,DeathYear=2020 },
+                new famousPeople() {Name = "Bill Gates", BirthYear=1955 }
+        };
+            var after1900 = from s in stemPeople
+                         where s.BirthYear > 1900
+                select s;
+            foreach(famousPeople person in after1900)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.WriteLine("-----------------------------------");
+            var nameLs = from s in stemPeople
+                         where s.Name.Contains("ll")
+                select s;
+            foreach (famousPeople person in nameLs)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.WriteLine("-----------------------------------");
+            var num1950 = stemPeople.Count(s => s.BirthYear > 1950);
+            Console.WriteLine($"{num1950}");
+            var yrs19202000 = from s in stemPeople
+                        where s.BirthYear > 1920 && s.BirthYear < 2000
+                select s.Name;
+            int yrsCount = yrs19202000.Count();
+            foreach(string person in yrs19202000)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine($"There are {yrsCount} people in the query who were born between 1920 and 2000, exclusive.");
+            Console.WriteLine("-----------------------------------");
+            var sortYear = from s in stemPeople
+                           orderby s.BirthYear
+                select s;
+            foreach(famousPeople person in sortYear) { Console.WriteLine(person.Name); }
+            Console.WriteLine("-----------------------------------");
+            var sortDeath = from s in stemPeople
+                            where s.DeathYear > 1960 && s.DeathYear < 2015
+                            orderby s.Name ascending
+                select s;
+            foreach(famousPeople people in sortYear) { Console.WriteLine(people.Name); }
+        }
+    }
+}
---
+++ studentsQuery/studentsQuery/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentsQuery
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+            public string Major { get; set; }
+            public double Tuition { get; set; }
+        }
+        public class StudentClubs
+        {
+            public int StudentID { get; set; }
+            public string ClubName { get; set; }
+        }
+        public class StudentGPA
+        {
+            public int StudentID { get; set; }
+            public double GPA { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            // Student collection
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "Frank Furter", Age = 55, Major="Hospitality", Tuition=3500.00} ,
+                new Student() { StudentID = 2, StudentName = "Gina Host", Age = 21, Major="Hospitality", Tuition=4500.00 } ,
+                new Student() { StudentID = 3, StudentName = "Cookie Crumb",  Age = 21, Major="CIT", Tuition=2500.00 } ,
+                new Student() { StudentID = 4, StudentName = "Ima Script",  Age = 48, Major="CIT", Tuition=5500.00 } ,
+                new Student() { StudentID = 5, StudentName = "Cora Coder",  Age = 35, Major="CIT", Tuition=1500.00 } ,
+                new Student() { StudentID = 6, StudentName = "Ura Goodchild" , Age = 40, Major="Marketing", Tuition=500.00} ,
+                new Student() { StudentID = 7, StudentName = "Take Mewith" , Age = 29, Major="Aerospace Engineering", Tuition=5500.00 }
+            };
+            // Student GPA Collection
+            IList<StudentGPA> studentGPAList = new List<StudentGPA>() {
+                new StudentGPA() { StudentID = 1,  GPA=4.0} ,
+                new StudentGPA() { StudentID = 2,  GPA=3.5} ,
+                new StudentGPA() { StudentID = 3,  GPA=2.0 } ,
+                new StudentGPA() { StudentID = 4,  GPA=1.5 } ,
+                new StudentGPA() { StudentID = 5,  GPA=4.0 } ,
+                new StudentGPA() { StudentID = 6,  GPA=2.5} ,
+                new StudentGPA() { StudentID = 7,  GPA=1.0 }
+            };
+            // Club collection
+            IList<StudentClubs> studentClubList = new List<StudentClubs>() {
+            new StudentClubs() {StudentID=1, ClubName="Photography" },
+            new StudentClubs() {StudentID=1, ClubName="Game" },
+            new StudentClubs() {StudentID=2, ClubName="Game" },
+            new StudentClubs() {StudentID=5, ClubName="Photography" },
+            new StudentClubs() {StudentID=6, ClubName="Game" },
+            new StudentClubs() {StudentID=7, ClubName="Photography" },
+            new StudentClubs() {StudentID=3, ClubName="PTK" },
+            };
+            var groupGPAIDs = studentGPAList.GroupBy(s => s.GPA);
+            foreach (var student in groupGPAIDs)
+            {
+                foreach (StudentGPA id in student)
+                {
+                    Console.WriteLine(id.StudentID);
+                }
+            }
+            var groupClubIDs = studentClubList.OrderBy(s => s.ClubName).GroupBy(s => s.ClubName);
+            foreach (var student in groupClubIDs)
+            {
+                foreach (StudentClubs id in student)
+                {
+                    Console.WriteLine(id.StudentID);
+                }
+            }
+            var numBet254 = studentGPAList.Count(s => s.GPA < 4.0 && s.GPA > 2.5);
+            Console.WriteLine(numBet254);
+            var avgTuition = studentList.Average(s => s.Tuition);
+            Console.WriteLine(avgTuition);
+            var maxTuition = studentList.Max(s => s.Tuition);
+            foreach (Student student in studentList)
+            {
+                if (student.Tuition >= maxTuition)
+                {
+                    Console.WriteLine(student.Major);
+                    Console.WriteLine(student.Tuition);
+                    Console.WriteLine(student.StudentName);
+                }
+            }
+            var joinStudentGPA = studentList.Join(studentGPAList, s => s.StudentID, g => g.StudentID, (s, g) => new
+            {
+                Name = s.StudentName,
+                Major = s.Major,
+                GPA = g.GPA
+            });
+            foreach (var student in joinStudentGPA)
+            {
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.Major);
+                Console.WriteLine(student.GPA);
+            }
+            var joinStudentClub = studentList.Join(studentClubList, s => s.StudentID, g => g.StudentID, (s, g) => new
+            {
+                Name = s.StudentName,
+                Club = g.ClubName
+            });
+            foreach (var student in joinStudentClub)
+            {
+                if (student.Club == "Game")
+                    Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
